2cbdcbbd9cddf221f5e880e62554208acfe4b55e
Moroz Sofiia <sofiia.moroz@allianzdirect.nl>
2022-05-31 12:45:09 +0200

    continue set up all components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test ', 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/5247ee12228109.56032d8b1b3b0.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test ', 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/5247ee12228109.56032d8b1b3b0.jpg')
+  ];
 
   constructor() { }
 
